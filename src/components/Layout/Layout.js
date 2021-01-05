e494a2f82fbd724d27c35bed1ab0380f78c22dc7
uzor1997 <66738486+uzor1997@users.noreply.github.com>
2021-01-05 23:44:23 +0100

    adding drawerToggle file

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxillary from '../../hoc/Auxillary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxillary>
-        <Toolbar />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxillary>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: false,
+    }
 
-export default layout;
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    SideDrawerToggleHandler = () => {
+        this.setState(prevState => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        })
+    }
+
+    render () {
+        return (
+            <Auxillary>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler}/>
+                <Toolbar drawerToggleClicked={this.SideDrawerToggleHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Auxillary>
+        );
+    }
+} 
+
+export default Layout;
