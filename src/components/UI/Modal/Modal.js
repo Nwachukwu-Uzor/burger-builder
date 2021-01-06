dd50d98fbd87384d1fa80aad146b24a8558150bd
uzor1997 <66738486+uzor1997@users.noreply.github.com>
2021-01-06 15:03:17 +0100

    improving project structure

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.module.css';
-import Auxillary from '../../../hoc/Auxillary';
+import Auxillary from '../../../hoc/Auxillary/Auxillary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = props => (
-    <Auxillary>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div 
-            className={classes.Modal}
-            style={{
-                transform: props.show ? "translateY(0)" :"translateY(-100vh)",
-                opacity: props.show ? "1" : "0",
-            }}
-        >
-            {props.children}
-        </div>
-    </Auxillary>
-);
+class Modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show
+        // if (nextProps.show !== this.props.show) {
+        //     return true;
+        // }
+    }
 
-export default modal;
+    componentDidUpdate() {
+        console.log("[Modal] componentDidUpdate")
+    }
+
+    render () {
+      return  (
+            <Auxillary>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div 
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? "translateY(0)" :"translateY(-100vh)",
+                        opacity: this.props.show ? "1" : "0",
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Auxillary>
+        );
+    }
+        
+        
+}
+
+export default Modal;
