5506cd502ec7e091521194f15bf1769f755768ba
uzor1997 <66738486+uzor1997@users.noreply.github.com>
2021-01-16 15:31:35 +0100

    refactoring the reducers

--- src/store/reducers/burgerBuilderReducer.js
+++ src/store/reducers/burgerBuilderReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionsTypes";
+import { updateObject } from "../utility";
 
 const initialState = {
     ingredients: null,
@@ -15,9 +16,24 @@ const INGREDIENTS_PRICES = {
 
 const reducer = (state=initialState, action) => {
     switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
+                totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
+            }
+            return updateObject(state, updatedState)
+        case actionTypes.REMOVE_INGREDIENT:
+            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+            const updatedIngs = updateObject(state.ingredients, updatedIng);
+            const updatedSt = {
+                ingredients: updatedIngs,
+                totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
+            }
+            return updateObject(state, updatedSt)
         case actionTypes.SET_INGREDIENTS:
-            return {
-                ...state,
+            return updateObject(state, {
                 ingredients: {
                     salad: action.ingredients.salad,
                     bacon: action.ingredients.bacon,
@@ -26,30 +42,11 @@ const reducer = (state=initialState, action) => {
                 },
                 totalPrice: 4,
                 error: false
-            }
+            })
         case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return {
-                ...state,
+            return updateObject(state, {
                 error: true
-            }
-        case actionTypes.ADD_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
-                totalPrice: state.totalPrice + INGREDIENTS_PRICES[action.ingredientName]
-            };
-        case actionTypes.REMOVE_INGREDIENT:
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENTS_PRICES[action.ingredientName]
-            }
+            })
         default:
             return state;
     }
